e5731033b26f92dd0eac289bc6d51fda188d9f14
Jd4rc3 <jdarce91@misena.edu.co>
2022-08-30 23:55:42 -0500

    Reactive form con validacion custom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewGameComponent } from './modules/game/pages/new-game/new-game.component';
 
 @NgModule({
   declarations: [AppComponent, NewGameComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [ReactiveFormsModule, BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/modules/game/pages/new-game/new-game.component.ts
+++ src/app/modules/game/pages/new-game/new-game.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { compileDeclareInjectorFromMetadata } from '@angular/compiler';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { PlayerModel } from '../../models/player.model';
 import { PlayerService } from '../../services/player.service';
 
 @Component({
@@ -7,13 +16,29 @@ import { PlayerService } from '../../services/player.service';
   styleUrls: ['./new-game.component.scss'],
 })
 export class NewGameComponent implements OnInit {
-  players;
+  players: PlayerModel[];
+  form: FormGroup;
 
   constructor(private playerService: PlayerService) {
     this.players = this.playerService.getPlayers();
+    this.form = this.createForm();
   }
 
-  ngOnInit(): void {
-    console.log(this.players);
+  ngOnInit(): void {}
+
+  submit(): void {
+    console.log(this.form.get('players')?.errors);
+  }
+
+  private createForm(): FormGroup {
+    return new FormGroup({
+      players: new FormControl('', [Validators.required, this.minPlayers]),
+    });
+  }
+
+  private minPlayers(control: AbstractControl): ValidationErrors | null {
+    return control.value.length < 2
+      ? { error: 'You must select at least 2 players' }
+      : null;
   }
 }
